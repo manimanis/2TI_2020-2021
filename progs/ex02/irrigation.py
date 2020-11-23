689439fbf553f516b51cf1ba6075d51618563e50
Mohamed Anis MANI <manimohamed@gmail.com>
2020-11-23 21:17:36 +0100

    Mise à jour projet n°2 Ajout QCM n°2

--- progs/ex02/irrigation.py
+++ progs/ex02/irrigation.py
@@ -6,12 +6,15 @@ t = int(input("Quel est le temps d'irrigation (mn) ? "))
 tp1 = t
 if t > 60:
     tp1 = 60
+tp2 = t
 
 print(f"Temps de fonct. pompe 1 : {tp1} mn")
 print(f"Temps de fonct. pompe 2 : {t} mn")
 
-qe1 = tp1 * 100
-qe2 = t * 80
+dp1 = 100
+dp2 = 80
+qe1 = tp1 * dp1
+qe2 = tp2 * dp2
 qe = qe1 + qe2
 
 print(f"La quantité d'eau pompée est : {qe1} + {qe2} = {qe} litres")
