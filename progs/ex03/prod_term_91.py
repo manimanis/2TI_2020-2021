1ab8e27bb70d9c4f56dcc61d7fa22696bdeab713
Mohamed Anis MANI <manimohamed@gmail.com>
2020-11-27 05:17:26 +0100

    Correction partielle exercices 3

--- progs/ex03/prod_term_91.py
+++ progs/ex03/prod_term_91.py
@@ -1,5 +1,5 @@
 for i in range(10, 100):
-    for j in range(i, 100):
+    for j in range(max(1000 // i, i+1), 100):
         p = i * j
         if p >= 1000 and p % 100 == 91:
             print(i, '*', j, '=', p)
